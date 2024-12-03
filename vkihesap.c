52a2ce71cb833c640986a758fb9fab742c0b3197
Emirhan AKMA <makma1@ogr.iu.edu.tr>
2024-12-03 23:22:13 +0300

    Update vkihesap.c

--- vkihesap.c
+++ vkihesap.c
@@ -9,11 +9,16 @@ int main(){
     scanf("%lf",&kilo);
     printf("Boyunuzu giriniz(metre):");
     scanf("%lf",&boy);
+    if(boy<=0 || kilo<=0) {
+        printf("Geçersiz bir değer girdiniz!\n");
+        return 1;
+    }
+    
     vki= vkihesap(kilo,boy);
-    if(vki<=18.5) printf("VKİ değeriniz %.2lf Durumunuz Zayıf",vki);
-    else if(vki>18.5 & vki<=24.9) printf("VKİ değeriniz %.2lf Durumunuz Normal",vki);
-    else if(vki>25 & vki<=29.9) printf("VKİ değeriniz %.2lf Durumunuz Fazla Kilolu",vki);
-    else if(vki>30) printf("VKİ değeriniz %.2lf Durumunuz Obez",vki);
-    else printf("Hata!!!");
+    if(vki<=18.5) printf("VKİ değeriniz %.2lf Durumunuz Zayıf\n",vki);
+    else if(vki>18.5 && vki<=24.9) printf("VKİ değeriniz %.2lf Durumunuz Normal\n",vki);
+    else if(vki>25 && vki<=29.9) printf("VKİ değeriniz %.2lf Durumunuz Fazla Kilolu\n",vki);
+    else if(vki>30) printf("VKİ değeriniz %.2lf Durumunuz Obez\n",vki);
+    else printf("Hata!!!\n");
     return 0;
     }
