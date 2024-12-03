4a6f04a7b6b3af30a84f79d8e971c280fa0bfa81
Emirhan AKMA <makma1@ogr.iu.edu.tr>
2024-12-04 01:03:24 +0300

    Update fahrenheittokelvin.c

--- fahrenheittokelvin.c
+++ fahrenheittokelvin.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-double cevirici(double fahrenheit){
-    return (fahrenheit+459.67)*5/9;
-}
+
+double cevirici(double fahrenheit)
+
 int main(){
     double fahrenheit,kelvin;
     printf("Kelvin e çevirmek istediğiniz Fahrenheit değerini giriniz:");
@@ -10,4 +10,8 @@ int main(){
     kelvin =cevirici(fahrenheit);
     printf("%.2lf Fahrenheit, %.2lf Kelvin eder.\n",fahrenheit,kelvin);
     return 0;
+}
+
+double cevirici(double fahrenheit){
+    return (fahrenheit+459.67)*5/9;
 }
