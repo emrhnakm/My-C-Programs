c857a1bdb2fb4a343ed92ac60650b9d40b0ad2fe
Emirhan AKMA <makma1@ogr.iu.edu.tr>
2024-12-03 21:35:42 +0300

    Added Fahrenheit to Kelvin conversion program

---
+++ fahrenheittokelvin.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+double cevirici(double fahrenheit){
+    return (fahrenheit+459.67)*5/9;
+}
+int main(){
+    double fahrenheit,kelvin;
+    printf("Kelvin e çevirmek istediğiniz Fahrenheit değerini giriniz:");
+    scanf("%lf",&fahrenheit);
+    kelvin =cevirici(fahrenheit);
+    printf("%.2lf Fahrenheit, %.2lf Kelvin eder.\n",fahrenheit,kelvin);
+    return 0;
+}
